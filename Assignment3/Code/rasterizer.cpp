78ae50f71118fe2c2761394e0027d219ce41fa89
Saltsuica <740050093@qq.com>
2021-03-09 10:54:46 +0800

    Fix view pos calculate

--- Assignment3/Code/rasterizer.cpp
+++ Assignment3/Code/rasterizer.cpp
@@ -271,19 +271,16 @@ static Eigen::Vector2f interpolate(float alpha, float beta, float gamma, const E
     return Eigen::Vector2f(u, v);
 }
 
-static Eigen::Vector3f interpolate(float alpha, float beta, float gamma, const Eigen::Vector4f& vert1, const Eigen::Vector4f& vert2, 
-    const Eigen::Vector4f& vert3, float weight)
-{
-    auto tmp = (alpha * vert1 + beta * vert2 + gamma * vert3) / weight;
-    return Vector3f(tmp[0]/tmp[3], tmp[1]/tmp[3], tmp[2]/tmp[3]);
-}
+// static Eigen::Vector3f interpolate(float alpha, float beta, float gamma, const Eigen::Vector4f& vert1, const Eigen::Vector4f& vert2, 
+//     const Eigen::Vector4f& vert3, float weight)
+// {
+//     auto tmp = (alpha * vert1 + beta * vert2 + gamma * vert3) / weight;
+//     return Vector3f(tmp[0]/tmp[3], tmp[1]/tmp[3], tmp[2]/tmp[3]);
+// }
 
 //Screen space rasterization
 void rst::rasterizer::rasterize_triangle(const Triangle& t, const std::array<Eigen::Vector3f, 3>& view_pos) 
 {
-    // TODO: From your HW3, get the triangle rasterization code.
-    // TODO: Inside your rasterization loop:
-
     // Find out the bounding box of current triangle.
     auto v = t.toVector4();
     float topf = -1, bottomf = INFINITY, leftf = INFINITY, rightf = -1;//box
@@ -309,25 +306,25 @@ void rst::rasterizer::rasterize_triangle(const Triangle& t, const std::array<Eig
             //    * v[i].w() is the vertex view space depth value z.
             //    * Z is interpolated view space depth for the current pixel
             //    * zp is depth between zNear and zFar, used for z-buffer
-            auto[alpha, beta, gamma] = computeBarycentric2D(x+0.5f, y+0.5f, t.v);
+            auto[alpha, beta, gamma] = computeBarycentric2D(x+0.5, y+0.5, t.v);
             float Z = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
             float zp = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
             zp *= Z;
 
             int depth_idx = get_index(x, y);
-            if (insideTriangle(x+0.5f, y+0.5f, t.v) && depth_buf[depth_idx] > zp)
+            //FIXME: should pass pixel center
+            if (insideTriangle(x, y, t.v) && depth_buf[depth_idx] > zp)
             {
                 depth_buf[depth_idx] = zp;
-                // TODO: Interpolate the attributes:
                 auto interpolated_color = interpolate(alpha, beta, gamma, t.color[0], t.color[1], t.color[2], 1.0);
                 auto interpolated_normal = interpolate(alpha, beta, gamma, t.normal[0], t.normal[1], t.normal[2], 1.0);
                 auto interpolated_texcoords = interpolate(alpha, beta, gamma, t.tex_coords[0], t.tex_coords[1], t.tex_coords[2], 1.0);
                 
-                auto interpolated_shadingcoords = interpolate(alpha, beta, gamma, t.a(), t.b(), t.c(), 1);
+                auto interpolated_shadingcoords = interpolate(alpha, beta, gamma, view_pos[0], view_pos[1], view_pos[2], 1);
 
-                // Vector3f interpolated_shadingcoords(x+0.5, y+0.5, );
                 fragment_shader_payload payload( interpolated_color, interpolated_normal.normalized(), interpolated_texcoords, texture ? &*texture : nullptr);
                 payload.view_pos = interpolated_shadingcoords;
+
                 // Use: Instead of passing the triangle's color directly to the frame buffer, pass the color to the shaders first to get the final color;
                 auto pixel_color = fragment_shader(payload);
                 set_pixel(Vector2i(x, y), pixel_color);
